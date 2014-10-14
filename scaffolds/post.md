0c2d4d250d5152e8f7ed9f7f9cb57dceca5c6e7a
lazyowl <lazylazyowl@gmail.com>
2014-10-14 22:17:35 +0800

    Initialization

---
+++ scaffolds/post.md
@@ -0,0 +1,5 @@
+title: {{ title }}
+date: {{ date }}
+catagories:
+tags: [default]
+---
---
+++ update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# use "hexo init" to create a new hexo repository in a folder
+# DON'T forget to use "npm install" then
+
+# use "hexo new __name__" to create a new post
+
+# use "hexo server" at the root to view it on local web browser
+
+hexo clean
+hexo generate
+hexo deploy
+
